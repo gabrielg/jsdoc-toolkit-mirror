b68cdea8c6011408534cb12b872268c49d45ce85
micmath <micmath@c4b26cf2-672e-0410-81b6-737671b986b9>
2007-06-15 22:35:55 +0000

    git-svn-id: http://jsdoc-toolkit.googlecode.com/svn/trunk@29 c4b26cf2-672e-0410-81b6-737671b986b9

---
+++ test/data/test2.js
@@ -0,0 +1,18 @@
+// nested functions
+
+function Layout(){
+	this.init = function() {}
+	
+	/** an element of the layout */
+	this.Element = function(elName) {
+		this.expand = function() {
+			
+		};
+	};
+	this.Canvas = function(top, left, width, height) {
+		this.initiated = true;
+	}
+	function rotate() {
+	
+	}
+}

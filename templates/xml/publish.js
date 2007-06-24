2b526160e4a84cec13cb8cb4fed1aa3752f274bf
micmath <micmath@c4b26cf2-672e-0410-81b6-737671b986b9>
2007-06-24 09:05:37 +0000

    git-svn-id: http://jsdoc-toolkit.googlecode.com/svn/trunk@48 c4b26cf2-672e-0410-81b6-737671b986b9

--- templates/xml/publish.js
+++ templates/xml/publish.js
@@ -1,18 +1,4 @@
-function publish_begin(allFiles, context) {
-	LOG.inform("Publish begin.");
-	context.template = new JsPlate(context.t+"file.tmpl");
-	context.output = "<?xml version=\"1.0\"?>\n<files>\n";
-}
-
-function publish_each(file, context) {
-	LOG.inform("Publishing: "+file.path+".");
-	context.output += context.template.process(file);
-}
-
-function publish_finish(allFiles, context) {
-	context.output += "\n</files>\n";
-	if (context.d) {
-		IO.saveFile(context.d, "jsdoc.xml", context.output);
-	}
-	LOG.inform("Publish finished.");
+function publish(xmlout, opt) {
+	LOG.inform("Publishing...");
+	// That was easy... JsDoc Toolkit's native format is already XML.
 }
